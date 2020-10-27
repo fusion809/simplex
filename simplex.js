94fad1d4d94cf0525d2644e8f7d1cd5dd3561eda
Brenton Horne <brentonhorne77@gmail.com>
2020-10-28 04:40:54 +1000

    Alphabetizing funcs; +patch of attempt to remove tempStr global

--- simplex.js
+++ simplex.js
@@ -1,4 +1,49 @@
 /**
+ * Show the optimal solution for the problem.
+ * 
+ * @param A   Final A array.
+ * @param b   Final b array.
+ * @param x   Decision variable array.
+ * @param xB  Final xB array.
+ * @param zc  zj-cj value array.
+ * @return    Nothing, just writes the solution to tempStr global.
+ */
+function showSolution(A, b, x, xB, z, zc) {
+    tempStr += "Optimal solution is ";
+    // List basic variable values
+    for (let i = 0 ; i < xB.length; i++) {
+        tempStr += subscripts(xB[i], {isBold: false, isLeftArrow: false, 
+            isDownArrow: false, notRow: true}) + " = " + b[i] + ", ";
+    }
+    var k = 0;
+    var m = A.length;
+    var mn = A[0].length;
+    var n = mn - m;
+    // List values of non-basic variables
+    for (let i = 0 ; i < x.length; i++) {
+        if (!find(xB, x[i])) {
+            if (k != 0) {
+                tempStr += ", ";
+            }
+            tempStr += subscripts(x[i], {isBold: false, isLeftArrow: false, 
+                isDownArrow: false, notRow: true}) + " = " + 0;               
+            k++;
+            if (k == n) {
+                tempStr += " and " + katex.renderToString("z = " + z[mn]) + ".";
+            }
+        }
+    }
+    for (let i = 0; i < mn; i++) {
+        // Checks if a variable is NOT in the basis and for it zj-cj=0 
+        // (implying alternate solutions)
+        if (!find(xB, x[i]) && zc[i] == 0) {
+            tempStr += " Alternate solution exists."
+        }
+    }
+    document.getElementById("tableau").innerHTML = tempStr;
+}
+
+/**
  * Performs a singe iteration of simplex and calls genTableau on the previous
  * iteration. All inputs pertain to the values from the previous iteration of
  * simplex.
@@ -201,50 +246,6 @@ function simplexIterator(A, b, cj, x, xB) {
     finalV = findV(A);
 }
 
-/**
- * Show the optimal solution for the problem.
- * 
- * @param A   Final A array.
- * @param b   Final b array.
- * @param x   Decision variable array.
- * @param xB  Final xB array.
- * @param zc  zj-cj value array.
- * @return    Nothing, just writes the solution to tempStr global.
- */
-function showSolution(A, b, x, xB, z, zc) {
-    tempStr += "Optimal solution is ";
-    // List basic variable values
-    for (let i = 0 ; i < xB.length; i++) {
-        tempStr += subscripts(xB[i], {isBold: false, isLeftArrow: false, 
-            isDownArrow: false, notRow: true}) + " = " + b[i] + ", ";
-    }
-    var k = 0;
-    var m = A.length;
-    var mn = A[0].length;
-    var n = mn - m;
-    // List values of non-basic variables
-    for (let i = 0 ; i < x.length; i++) {
-        if (!find(xB, x[i])) {
-            if (k != 0) {
-                tempStr += ", ";
-            }
-            tempStr += subscripts(x[i], {isBold: false, isLeftArrow: false, 
-                isDownArrow: false, notRow: true}) + " = " + 0;               
-            k++;
-            if (k == n) {
-                tempStr += " and " + katex.renderToString("z = " + z[mn]) + ".";
-            }
-        }
-    }
-    for (let i = 0; i < mn; i++) {
-        // Checks if a variable is NOT in the basis and for it zj-cj=0 
-        // (implying alternate solutions)
-        if (!find(xB, x[i]) && zc[i] == 0) {
-            tempStr += " Alternate solution exists."
-        }
-    }
-    document.getElementById("tableau").innerHTML = tempStr;
-}
 /**
  * Apply simplexIterator() to arguments obtained from getParameters()
  * 
